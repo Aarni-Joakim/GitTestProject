36ba7d6b47a035b4a1553c4011831a8696334982
annarae <anna.raevskaia@metropolia.fi>
2021-02-16 11:09:13 +0200

    Variable

--- app/src/main/java/com/example/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/gittestproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int hannu_noob = 0;
+        int hannu_noob_dev = 0;
     }
 }
