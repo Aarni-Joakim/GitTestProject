116f9e0f2366d4b35ab1fefc9de3d868e57ec47b
aarnijoakim <aarni.joakim@gmail.com>
2021-02-16 11:08:22 +0200

    Added hannu variable2

--- app/src/main/java/com/example/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/gittestproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int hannu = 0;
+        int hannu_noob = 0;
     }
 }
